bb3e2d8459f820b67a8bff8a6be57ab96af8da8b
tinted-knight <the.tinted.knight@gmail.com>
2020-12-14 14:20:36 +0300

    RadioBrowserFragment.kt
     - extends ContourFragment

--- app/src/main/java/com/noomit/radioalarm02/ui/radio_browser/home/RadioBrowserFragment.kt
+++ app/src/main/java/com/noomit/radioalarm02/ui/radio_browser/home/RadioBrowserFragment.kt
@@ -5,26 +5,25 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ScrollView
-import androidx.fragment.app.Fragment
-import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.navGraphViewModels
+import com.noomit.playerservice.ContourFragment
 import com.noomit.radioalarm02.Application00
 import com.noomit.radioalarm02.R
 import com.noomit.radioalarm02.base.ViewModelFactory
+import com.noomit.radioalarm02.base.collect
 import com.noomit.radioalarm02.domain.server_manager.ServerState
 import com.noomit.radioalarm02.toast
 import com.noomit.radioalarm02.ui.radio_browser.RadioBrowserViewModel
 import com.squareup.contour.utils.children
 import kotlinx.coroutines.FlowPreview
-import kotlinx.coroutines.flow.collect
 import timber.log.Timber
 
 private fun plog(message: String) =
     Timber.tag("tagg-app").i("$message [${Thread.currentThread().name}]")
 
 @FlowPreview
-class RadioBrowserFragment : Fragment() {
+class RadioBrowserFragment : ContourFragment() {
 
     private val viewModel: RadioBrowserViewModel by navGraphViewModels(
         navGraphId = R.id.nav_radio_browser,
@@ -45,6 +44,10 @@ class RadioBrowserFragment : Fragment() {
 
     private val adapter by lazy(LazyThreadSafetyMode.NONE) { ServerListAdapter(viewModel::setServer) }
 
+    private val contour by lazy(LazyThreadSafetyMode.NONE) {
+        ((view as ViewGroup).children.first() as IRadioBrowserHomeLayout)
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -57,26 +60,24 @@ class RadioBrowserFragment : Fragment() {
         return scrollView
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
-        val view = ((view as ViewGroup).children.first() as IRadioBrowserHomeLayout).also {
-            it.delegate = listener
-            it.setServerAdapter(adapter)
-            it.showLoading()
+    override fun prepareView() {
+        contour.apply {
+            delegate = listener
+            setServerAdapter(adapter)
+            showLoading()
         }
+    }
 
-        lifecycleScope.launchWhenStarted {
-            viewModel.availableServers.collect {
-                when (it) {
-                    is ServerState.Loading -> {
-                    }
-                    is ServerState.Values -> {
-                        plog("server list")
-                        view.showContent(it.values)
-                    }
-                    is ServerState.Failure -> requireContext().toast(it.e.localizedMessage)
+    override fun observeViewModel() {
+        collect(viewModel.availableServers) {
+            when (it) {
+                is ServerState.Loading -> {
                 }
+                is ServerState.Values -> {
+                    plog("server list")
+                    contour.showContent(it.values)
+                }
+                is ServerState.Failure -> requireContext().toast(it.e.localizedMessage)
             }
         }
     }
