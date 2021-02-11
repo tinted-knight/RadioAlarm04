27837efc4d60883623cd83b8d2344c254e48ebdb
tinted-knight <the.tinted.knight@gmail.com>
2021-02-11 17:04:34 +0300

    [ui] HelpView
     - size back to 56dp to all screens
     - layout transition refactoring

--- app/src/main/java/com/noomit/radioalarm02/ui/alarm_list/HomeLayout.kt
+++ app/src/main/java/com/noomit/radioalarm02/ui/alarm_list/HomeLayout.kt
@@ -7,13 +7,10 @@ import android.graphics.drawable.GradientDrawable
 import android.util.AttributeSet
 import android.view.ContextThemeWrapper
 import android.view.View
-import android.view.animation.OvershootInterpolator
 import androidx.core.animation.doOnEnd
 import androidx.core.view.isVisible
-import androidx.interpolator.view.animation.FastOutSlowInInterpolator
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import androidx.transition.ChangeBounds
 import androidx.transition.TransitionManager
 import com.google.android.material.textview.MaterialTextView
 import com.noomit.domain.entities.AlarmModel
@@ -108,8 +105,7 @@ class HomeLayout(context: Context, attrSet: AttributeSet? = null) : ContourLayou
             topTo { parent.top() }.bottomTo { btnAddAlarm.top() }
         )
 
-        val densityDpi = resources.configuration.densityDpi
-        val fabSize = if (densityDpi < 460f) appTheme.helpView.fabSizeSmall else appTheme.helpView.fabSize
+        val fabSize = appTheme.helpView.fabSize
         helpView.layoutBy(
             rightTo { if (isHelpExpanded) parent.right() - 16.xdip else parent.right() - 16.xdip }
                 .widthOf { if (isHelpExpanded) parent.width() * 3 / 4 else fabSize.xdip },
@@ -133,12 +129,7 @@ class HomeLayout(context: Context, attrSet: AttributeSet? = null) : ContourLayou
     }
 
     private fun helpClick(view: View) {
-        TransitionManager.beginDelayedTransition(this,
-            ChangeBounds().apply {
-                duration = 300L
-                interpolator = if (view.isSelected) FastOutSlowInInterpolator() else OvershootInterpolator(1f)
-            }
-        )
+        TransitionManager.beginDelayedTransition(this, helpView.layoutTransition)
         view.isSelected = !view.isSelected
         requestLayout()
     }
